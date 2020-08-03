61c0709dddd675b59dabff10984a074f3379d5be
mge <mge@pixum.com>
2020-08-03 21:12:13 +0200

    Updating development environment

---
+++ styles.11ty.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+
+// the file name as an entry point for postcss compilation
+// also used to define the output filename in our output /css folder.
+const fileName = "styles.css";
+
+module.exports = class {
+  async data () {
+    const rawFilepath = path.join(__dirname, `./_includes/postcss/${fileName}`);
+    return {
+      permalink: `css/${fileName}`,
+      rawFilepath,
+      rawCss: `${await fs.readFileSync(rawFilepath)}`
+    };
+  };
+
+  async render ({ rawCss, rawFilepath }) {
+    return await postcss([
+     // require('precss'), 
+      require('postcss-import'),
+      require('postcss-extend'),
+    // require('postcss-mixins'),
+    // require('postcss-color-mix'),
+      require('cssnano'), 
+      autoprefixer({ grid: "autoplace", env:"last 3 version" }),
+      require('postcss-prettify')
+    ])
+    .process(rawCss, { from: rawFilepath })
+    .then(result => result.css);
+  };
+}
